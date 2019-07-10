4eb06e13555bdde7917041482d4a2f0d1e6c997c
Madhur <gmadhur777@gmail.com>
2019-07-10 12:29:53 +0530

    pandas.py

---
+++ pandas.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  1 12:41:02 2019
+
+@author: Madhur
+"""
+
+import pandas as pd
+data={'name':['mad','sam'],'age':[12,5]}
+df=pd.DataFrame(data)
+print(df)
